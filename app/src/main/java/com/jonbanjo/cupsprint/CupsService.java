6e626824a4cda9eb6c72aaef60e1b5dd31cd4a38
Benoit Duffez <benoit@bicou.net>
2015-06-06 13:41:42 +0200

    allow print jobs to be cancelled

--- app/src/main/java/com/jonbanjo/cupsprint/CupsService.java
+++ app/src/main/java/com/jonbanjo/cupsprint/CupsService.java
@@ -22,6 +22,7 @@
 package com.jonbanjo.cupsprint;
 
 import android.os.AsyncTask;
+import android.print.PrintJobId;
 import android.print.PrintJobInfo;
 import android.printservice.PrintJob;
 import android.printservice.PrintService;
@@ -30,25 +31,76 @@ import android.util.Log;
 
 import org.cups4j.CupsClient;
 import org.cups4j.CupsPrinter;
+import org.cups4j.PrintRequestResult;
 
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * CUPS print service
  */
 public class CupsService extends PrintService {
+	Map<PrintJobId, Integer> mJobs = new HashMap<>();
+
 	@Override
 	protected PrinterDiscoverySession onCreatePrinterDiscoverySession() {
 		return new CupsPrinterDiscoverySession(this);
 	}
 
 	@Override
-	protected void onRequestCancelPrintJob(PrintJob printJob) {
-		// TODO
+	protected void onRequestCancelPrintJob(final PrintJob printJob) {
+		Log.d("CUPS", "Cancel print job: " + printJob);
+		final PrintJobInfo jobInfo = printJob.getInfo();
+		String url = jobInfo.getPrinterId().getLocalId();
+		String clientUrl = url.substring(0, url.substring(0, url.lastIndexOf('/')).lastIndexOf('/'));
+		final int jobId = mJobs.get(printJob.getId());
+
+		try {
+			final URL clientURL = new URL(clientUrl);
+			new AsyncTask<Void, Void, Void>() {
+				@Override
+				protected Void doInBackground(Void... params) {
+					cancelPrintJob(clientURL, jobId);
+					return null;
+				}
+
+				@Override
+				protected void onPostExecute(Void v) {
+					onPrintJobCancelled(printJob);
+				}
+			}.execute();
+		} catch (MalformedURLException e) {
+			Log.e("CUPS", "Couldn't cancel print job: " + printJob + " because: " + e);
+		}
+	}
+
+	/**
+	 * Called from a background thread, ask the printer to cancel a job by its printer job ID
+	 *
+	 * @param clientURL The printer client URL
+	 * @param jobId     The printer job ID
+	 */
+	private void cancelPrintJob(URL clientURL, int jobId) {
+		try {
+			CupsClient client = new CupsClient(clientURL);
+			client.cancelJob(jobId);
+		} catch (Exception e) {
+			Log.e("CUPS", "Couldn't cancel job: " + jobId + " because: " + e);
+		}
+	}
+
+	/**
+	 * Called on the main thread, when the print job was cancelled
+	 *
+	 * @param printJob The print job
+	 */
+	private void onPrintJobCancelled(PrintJob printJob) {
+		printJob.cancel();
 	}
 
 	@Override
@@ -62,12 +114,13 @@ public class CupsService extends PrintService {
 			final URL printerURL = new URL(url);
 			final URL clientURL = new URL(clientUrl);
 			final FileDescriptor fd = printJob.getDocument().getData().getFileDescriptor();
+			final PrintJobId jobId = printJob.getId();
 
 			// Send print job
 			new AsyncTask<Void, Void, Void>() {
 				@Override
 				protected Void doInBackground(Void... params) {
-					printDocument(clientURL, printerURL, fd);
+					printDocument(jobId, clientURL, printerURL, fd);
 					return null;
 				}
 
@@ -88,14 +141,16 @@ public class CupsService extends PrintService {
 	 * @param printerURL The printer URL
 	 * @param fd         The document to print, as a {@link FileDescriptor}
 	 */
-	private void printDocument(URL clientURL, URL printerURL, FileDescriptor fd) {
+
+	private void printDocument(PrintJobId jobId, URL clientURL, URL printerURL, FileDescriptor fd) {
 		try {
 			CupsClient client = new CupsClient(clientURL);
 			CupsPrinter printer = client.getPrinter(printerURL);
 
 			InputStream is = new FileInputStream(fd);
 			org.cups4j.PrintJob job = new org.cups4j.PrintJob.Builder(is).build();
-			printer.print(job);
+			PrintRequestResult result = printer.print(job);
+			mJobs.put(jobId, result.getJobId());
 		} catch (Exception e) {
 			Log.e("CUPS", "Couldn't send file descriptor: " + fd + " to printer because: " + e);
 		}
