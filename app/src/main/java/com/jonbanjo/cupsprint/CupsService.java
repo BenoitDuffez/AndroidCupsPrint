518346134d0f5fb62562b6f58fbe0a5439378dd8
Benoit Duffez <benoit@bicou.net>
2015-06-06 12:09:52 +0200

    queued jobs are now sent to the printer

--- app/src/main/java/com/jonbanjo/cupsprint/CupsService.java
+++ app/src/main/java/com/jonbanjo/cupsprint/CupsService.java
@@ -21,9 +21,21 @@
 
 package com.jonbanjo.cupsprint;
 
+import android.os.AsyncTask;
+import android.print.PrintJobInfo;
 import android.printservice.PrintJob;
 import android.printservice.PrintService;
 import android.printservice.PrinterDiscoverySession;
+import android.util.Log;
+
+import org.cups4j.CupsClient;
+import org.cups4j.CupsPrinter;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * CUPS print service
@@ -40,7 +52,61 @@ public class CupsService extends PrintService {
 	}
 
 	@Override
-	protected void onPrintJobQueued(PrintJob printJob) {
+	protected void onPrintJobQueued(final PrintJob printJob) {
+		final PrintJobInfo jobInfo = printJob.getInfo();
+		String url = jobInfo.getPrinterId().getLocalId();
+		String clientUrl = url.substring(0, url.substring(0, url.lastIndexOf('/')).lastIndexOf('/'));
+
+		try {
+			// Prepare job
+			final URL printerURL = new URL(url);
+			final URL clientURL = new URL(clientUrl);
+			final FileDescriptor fd = printJob.getDocument().getData().getFileDescriptor();
+
+			// Send print job
+			new AsyncTask<Void, Void, Void>() {
+				@Override
+				protected Void doInBackground(Void... params) {
+					printDocument(clientURL, printerURL, fd);
+					return null;
+				}
+
+				@Override
+				protected void onPostExecute(Void v) {
+					onPrintJobSent(printJob);
+				}
+			}.execute();
+		} catch (MalformedURLException e) {
+			Log.e("CUPS", "Couldn't queue print job: " + printJob + " because: " + e);
+		}
 	}
 
+	/**
+	 * Called from a background thread, when the print job has to be sent to the printer.
+	 *
+	 * @param clientURL  The client URL
+	 * @param printerURL The printer URL
+	 * @param fd         The document to print, as a {@link FileDescriptor}
+	 */
+	private void printDocument(URL clientURL, URL printerURL, FileDescriptor fd) {
+		try {
+			CupsClient client = new CupsClient(clientURL);
+			CupsPrinter printer = client.getPrinter(printerURL);
+
+			InputStream is = new FileInputStream(fd);
+			org.cups4j.PrintJob job = new org.cups4j.PrintJob.Builder(is).build();
+			printer.print(job);
+		} catch (Exception e) {
+			Log.e("CUPS", "Couldn't send file descriptor: " + fd + " to printer because: " + e);
+		}
+	}
+
+	/**
+	 * Called on the main thread, when the job was sent to the printer
+	 *
+	 * @param printJob The print job
+	 */
+	private void onPrintJobSent(PrintJob printJob) {
+		printJob.start();
+	}
 }
