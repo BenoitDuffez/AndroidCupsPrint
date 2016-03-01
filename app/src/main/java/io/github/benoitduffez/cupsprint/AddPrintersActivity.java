aca9b4ff002b7726a7d1d22e333faf590441852b
Benoit Duffez <benoit@bicou.net>
2016-03-01 14:14:18 +0100

    added a feature that allows to search for printers just based off the server IP/hostname

--- app/src/main/java/io/github/benoitduffez/cupsprint/AddPrintersActivity.java
+++ app/src/main/java/io/github/benoitduffez/cupsprint/AddPrintersActivity.java
@@ -2,10 +2,18 @@ package io.github.benoitduffez.cupsprint;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Called when the system needs to manually add a printer
@@ -31,12 +39,13 @@ public class AddPrintersActivity extends Activity {
 	 */
 	public static final String PREF_NAME = "name";
 
-	EditText mUrl, mName;
+	EditText mUrl, mName, mServerIp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_printers);
+		mServerIp = (EditText) findViewById(R.id.add_server_ip);
 		mUrl = (EditText) findViewById(R.id.add_url);
 		mName = (EditText) findViewById(R.id.add_name);
 	}
@@ -67,5 +76,73 @@ public class AddPrintersActivity extends Activity {
 				finish();
 			}
 		}, 200);
+	}
+
+	public void searchPrinters(View button) {
+		new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(Void... params) {
+				searchPrinters("http");
+				searchPrinters("https");
+				return null;
+			}
+		}.execute();
+	}
+
+	/**
+	 * Will search for printers at the scheme://xxx/printers/ URL
+	 *
+	 * @param scheme The target scheme, http or https
+	 */
+	private void searchPrinters(String scheme) {
+		HttpURLConnection urlConnection = null;
+		StringBuilder sb = new StringBuilder();
+		String server = mServerIp.getText().toString();
+		if (!server.contains(":")) {
+			server += ":631";
+		}
+		final String baseUrl = String.format(Locale.ENGLISH, "%s://%s/printers/", scheme, server);
+		try {
+			urlConnection = (HttpURLConnection) new URL(baseUrl).openConnection();
+			InputStreamReader isw = new InputStreamReader(urlConnection.getInputStream());
+			int data = isw.read();
+			while (data != -1) {
+				char current = (char) data;
+				sb.append(current);
+				data = isw.read();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		} finally {
+			if (urlConnection != null) {
+				urlConnection.disconnect();
+			}
+		}
+
+		/**
+		 * 1: URL
+		 * 2: Name
+		 * 3: Description
+		 * 4: Location
+		 * 5: Make and model
+		 * 6: Current state
+		 * pattern matching fields:                       1          2                  3               4                5              6
+		 */
+		Pattern p = Pattern.compile("<TR><TD><A HREF=\"([^\"]+)\">([^<]+)</A></TD><TD>([^<]+)</TD><TD>([^<]+)</TD><TD>([^<]+)</TD><TD>([^<]+)</TD></TR>\n");
+		Matcher matcher = p.matcher(sb);
+		String url, name;
+		SharedPreferences prefs = getSharedPreferences(SHARED_PREFS_MANUAL_PRINTERS, MODE_PRIVATE);
+		SharedPreferences.Editor editor = prefs.edit();
+		int id = prefs.getInt(PREF_NUM_PRINTERS, 0);
+		while (matcher.find()) {
+			url = (baseUrl + matcher.group(1)).replace("//", "/");
+			name = matcher.group(3);
+			editor.putString(PREF_URL + id, url);
+			editor.putString(PREF_NAME + id, name);
+			id++;
+		}
+		editor.putInt(PREF_NUM_PRINTERS, id);
+		editor.commit();
 	}
 }
