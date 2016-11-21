001a1ce6ed70948ad0660ac054739eded94cbccc
Benoit Duffez <benoit.duffez@upactivity.com>
2016-11-21 09:19:45 +0100

    removed references to fabric/crashlytics

--- app/src/main/java/io/github/benoitduffez/cupsprint/CupsPrintApp.java
+++ app/src/main/java/io/github/benoitduffez/cupsprint/CupsPrintApp.java
@@ -3,10 +3,6 @@ package io.github.benoitduffez.cupsprint;
 import android.app.Application;
 import android.content.Context;
 
-import com.crashlytics.android.Crashlytics;
-
-import io.fabric.sdk.android.Fabric;
-
 public class CupsPrintApp extends Application {
     public static final String LOG_TAG = "CUPS";
 
@@ -24,6 +20,5 @@ public class CupsPrintApp extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
-        Fabric.with(this, new Crashlytics());
     }
 }
