c9b49b4b2e9ee2ce74d4f925c295c02f0b7a9eb4
Solomon Peachy <pizza@shaftnet.org>
2017-03-11 22:28:42 -0500

    Don't blow away the host/device dictionaries with each packet.
    This allows a multi-packet response from a server to be handled
    properly.

--- app/src/main/java/io/github/benoitduffez/cupsprint/detect/MdnsServices.java
+++ app/src/main/java/io/github/benoitduffez/cupsprint/detect/MdnsServices.java
@@ -33,7 +33,12 @@ public class MdnsServices {
 
     private Exception exception = null;
 
+    private Map<String, String> hosts;
+    private Map<String, String[]> services;
+
     public MdnsServices() {
+            hosts = new HashMap<>();
+            services = new HashMap<>();
     }
 
     private byte[] makeQuestion(String data) {
@@ -80,7 +85,6 @@ public class MdnsServices {
             Iterator<? extends DNSRecord> iterator = answers.iterator();
             ServiceInfo info;
 
-            Map<String, String> hosts = new HashMap<>();
             while (iterator.hasNext()) {
                 DNSRecord record = iterator.next();
                 if (record instanceof DNSRecord.Address) {
@@ -90,7 +94,6 @@ public class MdnsServices {
                     iterator.remove();
                 }
             }
-            Map<String, String[]> services = new HashMap<>();
             iterator = answers.iterator();
             while (iterator.hasNext()) {
                 DNSRecord record = iterator.next();
